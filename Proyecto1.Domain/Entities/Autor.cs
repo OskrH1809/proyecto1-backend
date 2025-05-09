315a1b91f940d133371a396cec529ea66d0e648b
Oscar Canales <hoscar161@gmail.com>
2025-05-08 23:46:58 -0600

    Agrega entidades Autor y Libro al dominio

---
+++ Proyecto1.Domain/Entities/Autor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1.Domain.Entities
+{
+    internal class Autor
+    {
+        public int Id { get; set; }
+        public string NombreCompleto { get; set; } = null!;
+        public DateTime FechaNacimiento { get; set; }
+        public string? CiudadProcedencia { get; set; }
+        public string? CorreoElectronico { get; set; }
+
+        
+
+        public ICollection<Libro> Libros { get; set; } = new List<Libro>();
+    }
+}
---
+++ Proyecto1.Domain/Entities/Libro.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto1.Domain.Entities
+{
+    internal class Libro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = null!;
+        public int Anio { get; set; }
+        public string? Genero { get; set; }
+        public int? NumeroPaginas { get; set; }
+
+
+        public int AutorId { get; set; }
+        public Autor Autor { get; set; } = null!;
+    }
+}
