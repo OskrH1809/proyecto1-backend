3203ccc7a318f87aede895c8826464663b7a9521
Oscar Canales <hoscar161@gmail.com>
2025-05-09 00:18:36 -0600

    creacion de interfaz IUnitOfWork y AppDbContext

---
+++ Proyecto1.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto1.Domain.Entities;
+
+namespace Proyecto1.Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Autor> Autores => Set<Autor>();
+    public DbSet<Libro> Libros => Set<Libro>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Autor>(entity =>
+        {
+            entity.Property(a => a.NombreCompleto).IsRequired();
+            entity.Property(a => a.FechaNacimiento).IsRequired();
+        });
+
+        modelBuilder.Entity<Libro>(entity =>
+        {
+            entity.Property(l => l.Titulo).IsRequired();
+            entity.Property(l => l.Anio).IsRequired();
+        });
+    }
+}
