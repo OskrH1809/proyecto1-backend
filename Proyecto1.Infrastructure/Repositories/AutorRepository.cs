7622cf11fe7b3bc433319b8ce765cab204b67ae1
Oscar Canales <hoscar161@gmail.com>
2025-05-09 00:30:35 -0600

    creacion de repositorios de autor y libro en infraestructure

---
+++ Proyecto1.Infrastructure/Repositories/AutorRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto1.Domain.Entities;
+using Proyecto1.Domain.Interfaces;
+using Proyecto1.Infrastructure.Data;
+
+namespace Proyecto1.Infrastructure.Repositories;
+
+public class AutorRepository : IAutorRepository
+{
+    private readonly AppDbContext _context;
+
+    public AutorRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Autor>> GetAllAsync()
+    {
+        return await _context.Autores.ToListAsync();
+    }
+
+    public async Task<Autor?> GetByIdAsync(int id)
+    {
+        return await _context.Autores.FindAsync(id);
+    }
+
+    public async Task AddAsync(Autor autor)
+    {
+        await _context.Autores.AddAsync(autor);
+    }
+
+    public void Update(Autor autor)
+    {
+        _context.Autores.Update(autor);
+    }
+
+    public void Remove(Autor autor)
+    {
+        _context.Autores.Remove(autor);
+    }
+}
---
+++ Proyecto1.Infrastructure/Repositories/LibroRepository.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto1.Domain.Entities;
+using Proyecto1.Domain.Interfaces;
+using Proyecto1.Infrastructure.Data;
+
+namespace Proyecto1.Infrastructure.Repositories;
+
+public class LibroRepository : ILibroRepository
+{
+    private readonly AppDbContext _context;
+
+    public LibroRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Libro>> GetAllAsync()
+    {
+        return await _context.Libros.ToListAsync();
+    }
+
+    public async Task<Libro?> GetByIdAsync(int id)
+    {
+        return await _context.Libros.FindAsync(id);
+    }
+
+    public async Task AddAsync(Libro libro)
+    {
+        await _context.Libros.AddAsync(libro);
+    }
+
+    public void Update(Libro libro)
+    {
+        _context.Libros.Update(libro);
+    }
+
+    public void Remove(Libro libro)
+    {
+        _context.Libros.Remove(libro);
+    }
+}
