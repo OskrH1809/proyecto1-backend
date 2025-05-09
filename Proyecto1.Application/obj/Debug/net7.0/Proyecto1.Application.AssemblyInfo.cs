251dea1906873774e21988358e83e7daed226563
Oscar Canales <hoscar161@gmail.com>
2025-05-08 23:31:36 -0600

    Estructuracion de carpetas, limpieza de bin/obj/.vs y creacion de script para bd

--- Proyecto1.Application/obj/Debug/net7.0/Proyecto1.Application.AssemblyInfo.cs
+++ Proyecto1.Application/obj/Debug/net7.0/Proyecto1.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto1.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eec023cf2f1e6d6169b132dac07c92053f84b05")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto1.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto1.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
