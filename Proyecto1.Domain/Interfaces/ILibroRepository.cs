e66cd085acac242687fee27dc05cb30b4f56756f
Oscar Canales <hoscar161@gmail.com>
2025-05-08 23:58:00 -0600

    Creacion de interfaces para autor y libro

---
+++ Proyecto1.Domain/Interfaces/ILibroRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Proyecto1.Domain.Entities;
+
+namespace Proyecto1.Domain.Interfaces
+{
+    public interface ILibroRepository
+    {
+        Task<IEnumerable<Libro>> GetAllAsync();
+        Task<Libro?> GetByIdAsync(int id);
+        Task AddAsync(Libro libro);
+        void Update(Libro libro);
+        void Remove(Libro libro);
+    }
+}
